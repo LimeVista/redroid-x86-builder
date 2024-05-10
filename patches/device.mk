37f2bf96770a9d9d804abd32dd698c6435075fba
LimeVista <LimeVista@outlook.com>
2024-05-10 17:18:21 +0800

    测试

--- patches/device.mk
+++ patches/device.mk
@@ -6,6 +6,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 $(call inherit-product, device/redroid-prebuilts/prebuilts_x86.mk)
 $(call inherit-product, vendor/gapps/x86_64/x86_64-vendor.mk)
-$(call inherit-product-if-exists, vendor/google/proprietary/widevine-prebuilt/widevine.mk)
-$(call inherit-product-if-exists, vendor/intel/proprietary/houdini/houdini.mk)
-$(call inherit-product-if-exists, vendor/intel/proprietary/houdini/native_bridge_arm_on_x86.mk)
+$(call inherit-product, vendor/google/proprietary/widevine-prebuilt/widevine.mk)
+$(call inherit-product, vendor/intel/proprietary/houdini/houdini.mk)
+$(call inherit-product, vendor/intel/proprietary/houdini/native_bridge_arm_on_x86.mk)
