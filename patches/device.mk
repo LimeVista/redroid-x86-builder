b28a320df36a91f6c06726d3c02ea7db23cfb80a
LimeVista <LimeVista@outlook.com>
2024-05-10 19:11:27 +0800

    测试

--- patches/device.mk
+++ patches/device.mk
@@ -4,8 +4,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.nativebridge=1
 
+BOARD_SEPOLICY_DIRS += vendor/intel/proprietary/houdini/sepolicy
+
 $(call inherit-product, device/redroid-prebuilts/prebuilts_x86.mk)
 $(call inherit-product, vendor/gapps/x86_64/x86_64-vendor.mk)
-# $(call inherit-product, vendor/google/proprietary/widevine-prebuilt/widevine.mk)
+$(call inherit-product, vendor/google/proprietary/widevine-prebuilt/widevine.mk)
 $(call inherit-product, vendor/intel/proprietary/houdini/houdini.mk)
 $(call inherit-product, vendor/intel/proprietary/houdini/native_bridge_arm_on_x86.mk)
